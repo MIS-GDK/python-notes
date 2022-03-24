bb56fd68d1d0b814fecefcc4dc6af86f3aa7c2f9
mis-gdk <1530951440@qq.com>
2022-03-24 18:13:56 +0800

    20220324

--- numpy学习.py
+++ numpy学习.py
@@ -3,5 +3,12 @@ import numpy as np
 # a = np.array([[1, 2], [3, 3]], dtype=int)
 
 # 设置类型为整数
-y = np.zeros((5,), dtype=np.int)
+# y = np.zeros((5,), dtype=int)
+# print(y)
+
+y = np.arange(15).reshape(3, 5)
 print(y)
+print(y.ndim)
+print(y.shape)
+print(y.dtype)
+print(y.itemsize)
