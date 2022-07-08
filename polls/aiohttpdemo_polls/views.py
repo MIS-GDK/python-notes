2a702fe3cadb00076209f306729ea491f782835f
mis-gdk <1530951440@qq.com>
2022-07-08 18:07:18 +0800

    20220708

--- polls/aiohttpdemo_polls/views.py
+++ polls/aiohttpdemo_polls/views.py
@@ -4,11 +4,13 @@ import aiohttp_jinja2
 import db
 
 
-# @aiohttp_jinja2.template("index.html")
 async def index(request):
     async with request.app["db"].acquire() as conn:
         cursor = await conn.execute(db.question.select())
         records = await cursor.fetchall()
         questions = [dict(q) for q in records]
-        # return {"questions": questions}
-        return web.Response(body=b"<h1>Index</h1>")
+        return web.Response(text=str(questions))
+
+
+# async def index(request):
+#     return web.Response(text="Hello Aiohttp!!!")
